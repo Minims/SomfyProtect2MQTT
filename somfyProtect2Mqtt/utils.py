d3f40a103652454bd20685c9df184746ba6971f6
Minims <github@minims.fr>
2021-07-12 21:56:07 +0200

    [SomfyProtect2MQTT] Add Smoke Detector

--- somfyProtect2Mqtt/utils.py
+++ somfyProtect2Mqtt/utils.py
@@ -12,9 +12,7 @@ from yaml.parser import ParserError
 LOGGER = logging.getLogger(__name__)
 
 
-def setup_logger(
-    debug: bool = False, filename: str = "/var/log/somfyProtect.log"
-) -> None:
+def setup_logger(debug: bool = False, filename: str = "/var/log/somfyProtect.log") -> None:
     """Setup Logging
     Args:
         debug (bool, optional): True if debug enabled. Defaults to False.
@@ -61,7 +59,10 @@ def read_config_file(config_file: str) -> Dict[str, Any]:
 
 
 def close_and_exit(
-    robot, code: int = 0, signal: int = None, frame=None,
+    robot,
+    code: int = 0,
+    signal: int = None,
+    frame=None,
 ) -> None:  # pylint: disable=unused-argument
     """Close & Exit
 
