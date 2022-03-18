1be551bb44859b6d38bbf8dcbb84571a633defd2
Minims <github@minims.fr>
2022-03-18 21:58:00 +0100

    Add all known Websocket message as Doc

--- somfyProtect2Mqtt/somfy_protect/websocket/__init__.py
+++ somfyProtect2Mqtt/somfy_protect/websocket/__init__.py
@@ -71,16 +71,12 @@ class SomfyProtectWebsocket:
 
         message_json = json.loads(message)
         callbacks = {
-            "security_level_change": self.update_alarm_status,
+            "security_level_change": self.security_level_change,
             "alarm.trespass": self.alarm_trespass,
             "alarm.panic": self.alarm_panic,
             "alarm.end": self.alarm_end,
             "presence_out": self.update_keyfob_presence,
             "presence_in": self.update_keyfob_presence,
-            "device.status": self.device_status,
-            "site.device.testing.status": self.site_device_testing_status,
-            "device.update.connect": self.default_message,
-            "device.update.progress": self.default_message,
         }
 
         ack = {
@@ -89,6 +85,7 @@ class SomfyProtectWebsocket:
             "client": "Android",
         }
         ws_app.send(json.dumps(ack))
+        self.default_message(message_json)
         if message_json["key"] in callbacks:
             callbacks[message_json["key"]](message_json)
         else:
@@ -102,11 +99,37 @@ class SomfyProtectWebsocket:
         """Handle Websocket Close Connection"""
         LOGGER.info("Closing websocket connection")
         LOGGER.info("Reconnecting")
-        time.sleep(10)
+        time.sleep(2)
         self.run_forever()
 
     def update_keyfob_presence(self, message):
         """Update Key Fob Presence"""
+        # {
+        # "profiles":[
+        #     "owner",
+        #     "admin"
+        # ],
+        # "site_id":"XXX",
+        # "type":"event",
+        # "key":"presence_in",
+        # "user_id":"XXX",
+        # "device_id":"XXX",
+        # "device_type":"fob",
+        # "message_id":"XXX"
+        # },
+        # {
+        # "profiles":[
+        #     "owner",
+        #     "admin"
+        # ],
+        # "site_id":"XXX",
+        # "type":"event",
+        # "key":"presence_out",
+        # "user_id":"XXX",
+        # "device_id":"XXX",
+        # "device_type":"fob",
+        # "message_id":"XXX"
+        # }
         LOGGER.info("Update Key Fob Presence")
         site_id = message.get("site_id")
         device_id = message.get("device_id")
@@ -120,8 +143,21 @@ class SomfyProtectWebsocket:
 
         mqtt_publish(mqtt_client=self.mqtt_client, topic=topic, payload=payload, retain=True)
 
-    def update_alarm_status(self, message):
+    def security_level_change(self, message):
         """Update Alarm Status"""
+        # {
+        # "profiles":[
+        #     "owner",
+        #     "admin",
+        #     "guest",
+        #     "kid"
+        # ],
+        # "site_id":"XXX",
+        # "type":"config",
+        # "key":"security.level.change",
+        # "security_level":"armed",
+        # "message_id":"XXX"
+        # }
         LOGGER.info("Update Alarm Status")
         site_id = message.get("site_id")
         security_level = message.get("security_level")
@@ -132,6 +168,26 @@ class SomfyProtectWebsocket:
 
     def alarm_trespass(self, message):
         """Alarm Triggered !!"""
+        # {
+        # "profiles":[
+        #     "owner",
+        #     "admin",
+        #     "custom",
+        #     "family",
+        #     "neighbor"
+        # ],
+        # "site_id":"XXX",
+        # "type":"alarm",
+        # "key":"alarm.trespass",
+        # "device_id":"XXX",
+        # "device_type":"pir",
+        # "start_at":"2022-03-14T17:17:12.000000Z",
+        # "start_siren_at":"2022-03-14T17:17:42.000000Z",
+        # "end_at":"2022-03-14T17:20:42.000000Z",
+        # "end_siren_at":"2022-03-14T17:20:42.000000Z",
+        # "manual_alarm":false,
+        # "message_id":"XXX"
+        # }
         LOGGER.info("Report Alarm Triggered")
         site_id = message.get("site_id")
         device_id = message.get("device_id")
@@ -147,6 +203,26 @@ class SomfyProtectWebsocket:
 
     def alarm_panic(self, message):
         """Report Alarm Panic"""
+        # {
+        # "profiles":[
+        #     "owner",
+        #     "admin",
+        #     "custom",
+        #     "family",
+        #     "neighbor"
+        # ],
+        # "site_id":"XXX",
+        # "type":"alarm",
+        # "key":"alarm.panic",
+        # "device_id":null,
+        # "device_type":null,
+        # "start_at":"2022-03-14T17:21:07.000000Z",
+        # "start_siren_at":"2022-03-14T17:21:07.000000Z",
+        # "end_at":"2022-03-14T17:24:07.000000Z",
+        # "end_siren_at":"2022-03-14T17:24:07.000000Z",
+        # "manual_alarm":false,
+        # "message_id":"XXX"
+        # }
         LOGGER.info("Report Alarm Panic")
         site_id = message.get("site_id")
         security_level = "triggered"
@@ -157,37 +233,252 @@ class SomfyProtectWebsocket:
 
     def alarm_end(self, message):
         """Report Alarm Stop"""
+        # {
+        # "profiles":[
+        #     "owner",
+        #     "admin",
+        #     "custom",
+        #     "family",
+        #     "neighbor"
+        # ],
+        # "site_id":"XXX",
+        # "type":"alarm",
+        # "key":"alarm.end",
+        # "device_id":null,
+        # "device_type":null,
+        # "end_at":"2022-03-14T17:19:22.000000Z",
+        # "end_siren_at":null,
+        # "stopped_by_user_id":"XXX",
+        # "message_id":"XXX"
+        # }
         LOGGER.info("Report Alarm Stop")
         site_id = message.get("site_id")
         update_site(self.api, self.mqtt_client, self.mqtt_config, site_id)
 
     def device_status(self, message):
         """Update Device Status"""
-        LOGGER.info("Read Test message")
-        site_id = message.get("site_id")
-        device_id = message.get("device_id")
-        payload = message
-        topic = f"{self.mqtt_config.get('topic_prefix', 'somfyProtect2mqtt')}/{site_id}/{device_id}/test"
-        mqtt_publish(mqtt_client=self.mqtt_client, topic=topic, payload=payload)
+        # {
+        # "profiles":[
+        #     "admin",
+        #     "owner",
+        #     "installer_write"
+        # ],
+        # "site_id":"XXX",
+        # "type":"testing",
+        # "key":"device.status",
+        # "device_id":"XXX",
+        # "device_lost":false,
+        # "rlink_quality":-73,
+        # "rlink_quality_percent":75,
+        # "battery_level":100,
+        # "recalibration_required":false,
+        # "cover_present":true,
+        # "last_status_at":"2022-03-16T16:06:56.000000Z",
+        # "diagnosis":{
+        #     "is_everything_ok":true,
+        #     "problems":[
+        #     ]
+        # },
+        # "message_id":"XXX"
+        # }
 
     def site_device_testing_status(self, message):
         """Site Device Testing Status"""
-        LOGGER.info("Read Site Device Testing Status")
-        items = message.get("items")
-        site_id = message.get("site_id")
-        # = items.get("device_id")
-        device_id = ""
-        payload = items
-        topic = f"{self.mqtt_config.get('topic_prefix', 'somfyProtect2mqtt')}/{site_id}/{device_id}/wss"
-        mqtt_publish(mqtt_client=self.mqtt_client, topic=topic, payload=payload)
+        # {
+        # "profiles":[
+        #     "owner",
+        #     "admin"
+        # ],
+        # "site_id":"XXX",
+        # "type":"testing",
+        # "key":"site.device.testing.status",
+        # "diagnosis":{
+        #     "main_status":"ok",
+        #     "main_message":"diagnosis.ok",
+        #     "main_message_vars":{
+
+        #     },
+        #     "device_diagnosis_available":true,
+        #     "device_diagnosis_expired":false,
+        #     "items":[
+
+        #     ]
+        # },
+        # "message_id":"XXX"
+        # }
 
     def default_message(self, message):
-        """Unknown Message"""
-        LOGGER.info("#####################################")
-        LOGGER.info("Read Unknown Message")
-        LOGGER.info(f"Message: {message}")
-        # LOGGER.info(f"Site: {message.get('site_id')}")
-        # LOGGER.info(f"Device: {message.get('device_id')}")
-        # LOGGER.info(f"Key: {message.get('key')}")
-        # LOGGER.info(f"Items: {message.get('items')}")
-        LOGGER.info("#####################################")
+        """Default Message"""
+        LOGGER.info(f"[default] Read Message {message}")
+        topic_suffix = message.get("key")
+        site_id = message.get("site_id")
+        device_id = message.get("device_id")
+        if not site_id:
+            topic = f"{self.mqtt_config.get('topic_prefix', 'somfyProtect2mqtt')}/{topic_suffix}"
+        elif not device_id:
+            topic = f"{self.mqtt_config.get('topic_prefix', 'somfyProtect2mqtt')}/{site_id}/{topic_suffix}"
+        else:
+            topic = f"{self.mqtt_config.get('topic_prefix', 'somfyProtect2mqtt')}/{site_id}/{device_id}/{topic_suffix}"
+        mqtt_publish(mqtt_client=self.mqtt_client, topic=topic, payload=message)
+
+    def remote_unassigned(self, message):
+        """Remote Unassigned"""
+        # {
+        # "profiles":[
+        #     "owner",
+        #     "admin"
+        # ],
+        # "site_id":"XXX",
+        # "type":"config",
+        # "key":"remote_unassigned",
+        # "user_id":"XXX",
+        # "device_id":"XXX",
+        # "message_id":"XXX"
+        # }
+
+    def device_firmware_update_fail(self, message):
+        """Device Firmware Update Fail"""
+        # {
+        # "profiles":[
+        #     "owner",
+        #     "admin"
+        # ],
+        # "site_id":"XXX",
+        # "type":"config",
+        # "key":"device.firmware.update.fail",
+        # "device_id":"XXX",
+        # "reason":100,
+        # "message_id":"XXX"
+        # }
+
+    def site_privacy(self, message):
+        """Site Privacy"""
+        # {
+        # "profiles":[
+        #     "admin",
+        #     "owner",
+        #     "installer_write"
+        # ],
+        # "site_id":"XXX",
+        # "type":"event",
+        # "key":"site.privacy",
+        # "active":true,
+        # "message_id":"XXX"
+        # }
+
+    def camerastatus_shutter(self, message):
+        """Camera Status Shutter Close"""
+        # {
+        # "profiles":[
+        #     "admin",
+        #     "owner",
+        #     "installer_write"
+        # ],
+        # "site_id":"XXX",
+        # "type":"config",
+        # "key":"camerastatus.shutter.close",
+        # "device_id":"XXX",
+        # "message_id":"XXX"
+        # }
+        # {
+        # "profiles":[
+        #     "admin",
+        #     "owner",
+        #     "installer_write"
+        # ],
+        # "site_id":"XXX",
+        # "type":"config",
+        # "key":"camerastatus.shutter.open",
+        # "device_id":"XXX",
+        # "message_id":"XXX"
+        # }
+
+    def snapshot_ready(self, message):
+        """Snapshot Ready"""
+        # {
+        # "profiles":[
+        #     "owner"
+        # ],
+        # "site_id":"XXX",
+        # "key":"snapshotready",
+        # "snapshot_id":"XXX",
+        # "device_id":"XXX",
+        # "snapshot_url":"https:\/\/video-cdn.myfox.io\/camera_snapshot\/XXX\/XXX.XXX-s?Expires=1647629662&Signature=XXX-XXX~XXX~XXX~XXX~XXX-XXX~XXX~XXX&Key-Pair-Id=XXX",
+        # "message_id":"XXX",
+        # "type":"event"
+        # }
+
+    def box_update_progress(self, message):
+        """Box Update Progress"""
+        # {
+        # "profiles":[
+        #     "owner",
+        #     "admin"
+        # ],
+        # "site_id":"XXX",
+        # "type":"config",
+        # "key":"box.update.progress",
+        # "box_id":"XXX",
+        # "progress":100,
+        # "remaining":0,
+        # "total":0,
+        # "update":"no update",
+        # "message_id":"XXX"
+        # }
+
+    def device_offline(self, message):
+        """Device Offline"""
+        # {
+        # "profiles":[
+        #     "owner",
+        #     "admin"
+        # ],
+        # "site_id":"XXX",
+        # "type":"event",
+        # "key":"device.offline",
+        # "device_id":"XXX",
+        # "message_id":"XXX"
+        # }
+
+    def device_update_connect(self, message):
+        """Device Update Connect"""
+        # {
+        # "profiles":[
+        #     "owner",
+        #     "admin"
+        # ],
+        # "site_id":"XXX",
+        # "type":"config",
+        # "key":"device.update.connect",
+        # "device_type":"mss_plug",
+        # "device_mac":"XXX",
+        # "message_id":"XXX"
+        # }
+
+    def diagnosis_connection_online_camera(self, message):
+        """Diagnosis Connection Online Camera"""
+        # {
+        # "profiles":[
+        #     "owner",
+        #     "admin"
+        # ],
+        # "site_id":"XXX",
+        # "type":"event",
+        # "key":"diagnosis.connection.online.camera",
+        # "device_id":"XXX",
+        # "message_id":"XXX"
+        # }
+
+    def diagnosis_connection_offline_camera(self, message):
+        """Diagnosis Connection Offline Camera"""
+        # {
+        # "profiles":[
+        #     "owner",
+        #     "admin"
+        # ],
+        # "site_id":"XXX",
+        # "type":"event",
+        # "key":"diagnosis.connection.offline.camera",
+        # "device_id":"XXX",
+        # "message_id":"XX"
+        # }
