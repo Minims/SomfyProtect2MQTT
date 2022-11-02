c20d4452cb4050531c917103048a6139360fea3e
Minims <github@minims.fr>
2022-11-02 12:29:35 +0100

    Update version to 0.2.4
    Update version to 0.2.4

--- somfyProtect2Mqtt/main.py
+++ somfyProtect2Mqtt/main.py
@@ -14,7 +14,7 @@ from somfy_protect.sso import init_sso
 from somfy_protect.api import SomfyProtectApi
 from somfy_protect.websocket import SomfyProtectWebsocket
 
-VERSION = "0.2.3"
+VERSION = "0.2.4"
 
 
 def somfy_protect_loop(somfy_protect_2_mqtt):
