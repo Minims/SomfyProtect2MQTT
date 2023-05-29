2f2018a780feee72683ec438bb5652e563adf8e9
Minims <github@minims.fr>
2023-05-29 10:48:49 +0200

    Set version to 2023.6.0

--- somfyProtect2Mqtt/main.py
+++ somfyProtect2Mqtt/main.py
@@ -14,7 +14,7 @@ from somfy_protect.sso import init_sso
 from somfy_protect.api import SomfyProtectApi
 from somfy_protect.websocket import SomfyProtectWebsocket
 
-VERSION = "2023.3.0"
+VERSION = "2023.6.0"
 
 
 def somfy_protect_loop(somfy_protect_2_mqtt):
