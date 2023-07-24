3b6f469220fdb29f29057a40405f66443c9c15e0
Minims <github@minims.fr>
2023-07-24 23:06:07 +0200

    Change threading to improve websocket

--- somfyProtect2Mqtt/main.py
+++ somfyProtect2Mqtt/main.py
@@ -5,6 +5,7 @@ import logging
 import threading
 from functools import partial
 from signal import SIGINT, SIGTERM, signal
+import time
 
 from exceptions import SomfyProtectInitError
 from somfy_protect_2_mqtt import SomfyProtect2Mqtt
@@ -14,25 +15,28 @@ from somfy_protect.sso import init_sso
 from somfy_protect.api import SomfyProtectApi
 from somfy_protect.websocket import SomfyProtectWebsocket
 
-VERSION = "2023.7.0"
+VERSION = "2023.7.1"
 
 
-def somfy_protect_loop(somfy_protect_2_mqtt):
+def somfy_protect_loop(config, mqtt_client, api):
     """SomfyProtect 2 MQTT Loop"""
     try:
-        somfy_protect_2_mqtt.loop()
-    except Exception as exp:
-        LOGGER.error(f"Force stopping Api {exp}")
-        close_and_exit(somfy_protect_2_mqtt, 3)
+        somfy_protect_api = SomfyProtect2Mqtt(api=api, mqtt_client=mqtt_client, config=config)
+        time.sleep(1)
+        somfy_protect_api.loop()
+    except SomfyProtectInitError as exc:
+        LOGGER.error(f"Force stopping Api {exc}")
+        close_and_exit(somfy_protect_api, 3)
 
 
-def somfy_protect_wss_loop(somfy_protect_websocket):
+def somfy_protect_wss_loop(sso, debug, config, mqtt_client, api):
     """SomfyProtect WSS Loop"""
     try:
-        somfy_protect_websocket.run_forever()
-    except Exception as exp:
-        LOGGER.error(f"Force stopping WebSocket {exp}")
-        close_and_exit(somfy_protect_websocket, 3)
+        wss = SomfyProtectWebsocket(sso=sso, debug=debug, config=config, mqtt_client=mqtt_client, api=api)
+        wss.run_forever()
+    except Exception as exc:
+        LOGGER.error(f"Force stopping WebSocket {exc}")
+        close_and_exit(wss, 3)
 
 
 if __name__ == "__main__":
@@ -54,24 +58,49 @@ if __name__ == "__main__":
     SSO = init_sso(config=CONFIG)
     API = SomfyProtectApi(sso=SSO)
     MQTT_CLIENT = init_mqtt(config=CONFIG, api=API)
-    WSS = SomfyProtectWebsocket(sso=SSO, debug=DEBUG, config=CONFIG, mqtt_client=MQTT_CLIENT, api=API)
-
-    try:
-        SOMFY_PROTECT = SomfyProtect2Mqtt(api=API, mqtt_client=MQTT_CLIENT, config=CONFIG)
-
-    except SomfyProtectInitError as exp:
-        LOGGER.error(f"Unable to init: {exp}")
-        close_and_exit(None, None, None, 1)
 
     # Trigger Ctrl-C
-    signal(SIGINT, partial(close_and_exit, SOMFY_PROTECT, 0))
-    signal(SIGTERM, partial(close_and_exit, SOMFY_PROTECT, 0))
+    signal(SIGINT, partial(close_and_exit, SomfyProtect2Mqtt, 0))
+    signal(SIGTERM, partial(close_and_exit, SomfyProtect2Mqtt, 0))
+    signal(SIGINT, partial(close_and_exit, SomfyProtectWebsocket, 0))
+    signal(SIGTERM, partial(close_and_exit, SomfyProtectWebsocket, 0))
 
     try:
-        p1 = threading.Thread(target=somfy_protect_loop, args=(SOMFY_PROTECT,))
-        p2 = threading.Thread(target=somfy_protect_wss_loop, args=(WSS,))
+        p1 = threading.Thread(
+            target=somfy_protect_loop,
+            args=(
+                CONFIG,
+                MQTT_CLIENT,
+                API,
+            ),
+        )
+        p2 = threading.Thread(
+            target=somfy_protect_wss_loop,
+            args=(
+                SSO,
+                DEBUG,
+                CONFIG,
+                MQTT_CLIENT,
+                API,
+            ),
+        )
         p1.start()
         p2.start()
+        p2.join()
+        while True:
+            if not p2.is_alive():
+                p2 = threading.Thread(
+                    target=somfy_protect_wss_loop,
+                    args=(
+                        SSO,
+                        DEBUG,
+                        CONFIG,
+                        MQTT_CLIENT,
+                        API,
+                    ),
+                )
+                p2.start()
+                time.sleep(1)
     except Exception as exp:
         LOGGER.error(f"Force stopping application {exp}")
-        close_and_exit(SOMFY_PROTECT, 3)
+        # close_and_exit(SOMFY_PROTECT, 3)
