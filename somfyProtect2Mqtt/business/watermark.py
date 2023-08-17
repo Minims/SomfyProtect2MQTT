16a0f55ca6ffe74946262a92a1403a8f96d8ea5f
Minims <github@minims.fr>
2023-08-17 15:09:02 +0200

    Add WaterMark on snapshot

--- somfyProtect2Mqtt/business/watermark.py
+++ somfyProtect2Mqtt/business/watermark.py
@@ -1,13 +1,19 @@
 """Add Watermark on Snapshot"""
+import logging
+import os
+
 from PIL import Image, ImageDraw, ImageFont
+
+LOGGER = logging.getLogger(__name__)
 
 
 def insert_watermark(file: str, watermark: str):
-    image = Image.open(file)
+    image = Image.open(
+        fp=file,
+    )
     watermark_image = image.copy()
     draw = ImageDraw.Draw(watermark_image)
     width, _ = image.size
-    font = ImageFont.truetype("Arial.ttf", 24)
+    font = ImageFont.truetype(f"{os.path.dirname(__file__)}/arial.ttf", 22)
     draw.text((width - 210, 0), watermark, fill=(0, 0, 0), font=font)
-    print("OK")
     watermark_image.save(file)
