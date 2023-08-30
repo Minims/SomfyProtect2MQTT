2978043dd07ade301f0ae5a73140d2487028cced
Minims <github@minims.fr>
2023-08-30 22:59:29 +0200

    2023.9.0

--- somfyProtect2Mqtt/main.py
+++ somfyProtect2Mqtt/main.py
@@ -13,7 +13,7 @@ from somfy_protect.sso import init_sso
 from somfy_protect.api import SomfyProtectApi
 from somfy_protect.websocket import SomfyProtectWebsocket
 
-VERSION = "2023.8.3"
+VERSION = "2023.9.0"
 
 
 def somfy_protect_loop(config, mqtt_client, api):
