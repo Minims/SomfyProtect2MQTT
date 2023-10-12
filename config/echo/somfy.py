f2015a7a1de51791bbc8c76a7ed0e4789c9f2a93
Minims <github@minims.fr>
2023-10-12 13:57:20 +0200

    feature: Allow camera streaming via MQTT Camera OR WebRTC Camera (go2rtc)

---
+++ config/echo/somfy.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Somfy Protect 2 MQTT Camera Stream with go2rtc
+https://github.com/AlexxIT/go2rtc/wiki/Source-Echo-examples#install-python-libraries
+"""
+
+import logging
+import sys
+
+LOGGER = logging.getLogger(__name__)
+FOLDER = "/config/somfyProtect2mqtt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] [%(name)s:%(lineno)d] %(message)s",
+    handlers=[
+        logging.FileHandler(filename=f"{FOLDER}/somfy.log"),
+    ],
+)
+
+LOGGER.info("Start")
+try:
+    with open(f"{FOLDER}/stream_url_{sys.argv[1]}", "r", encoding="utf-8") as url:
+        print(f"ffmpeg:{url.read()}#video=copy")
+        LOGGER.info(f"ffmpeg:{url.read()}#video=copy")
+except FileNotFoundError as exc:
+    LOGGER.warning(exc)
