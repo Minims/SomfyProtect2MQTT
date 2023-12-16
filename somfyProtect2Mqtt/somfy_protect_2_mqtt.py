6a24b1e72b27473101fe81c2dc422734ed5f35f4
Minims <github@minims.fr>
2023-12-16 19:05:07 +0100

    chore: update min site/device delay

--- somfyProtect2Mqtt/somfy_protect_2_mqtt.py
+++ somfyProtect2Mqtt/somfy_protect_2_mqtt.py
@@ -24,7 +24,9 @@ class SomfyProtect2Mqtt:
         SomfyProtectInitError: Unable to init
     """
 
-    def __init__(self, api: SomfyProtectApi, mqtt_client: MQTTClient, config: dict) -> None:
+    def __init__(
+        self, api: SomfyProtectApi, mqtt_client: MQTTClient, config: dict
+    ) -> None:
         """Init SomfyProtect2Mqtt
 
         Args:
@@ -41,10 +43,10 @@ class SomfyProtect2Mqtt:
         self.my_sites_id = []
 
         self.delay_site = config.get("delay_site", 60)
-        self.delay_site = max(self.delay_site, 10)
+        self.delay_site = max(self.delay_site, 60)
 
         self.delay_device = config.get("delay_device", 60)
-        self.delay_device = max(self.delay_device, 10)
+        self.delay_device = max(self.delay_device, 60)
 
         self.manual_snapshot = config.get("manual_snapshot", False)
 
@@ -65,7 +67,9 @@ class SomfyProtect2Mqtt:
                 LOGGER.info(f"Storing Site ID for {site.label}")
                 self.my_sites_id.append(site.id)
             else:
-                LOGGER.info(f"Site '{site.label}' is not set in configuration, Update it if you want to add this Site")
+                LOGGER.info(
+                    f"Site '{site.label}' is not set in configuration, Update it if you want to add this Site"
+                )
 
     def close(self) -> None:  # pylint: disable=no-self-use
         """Close"""
