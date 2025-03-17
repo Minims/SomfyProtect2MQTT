108640a5414368f04cb0b1f0e80dc11358203b29
Minims <github@minims.fr>
2025-03-17 21:06:17 +0100

    version: 2025.3.0

--- somfyProtect2Mqtt/main.py
+++ somfyProtect2Mqtt/main.py
@@ -16,7 +16,7 @@ from somfy_protect.sso import init_sso
 from somfy_protect.api import SomfyProtectApi
 from somfy_protect.websocket import SomfyProtectWebsocket
 
-VERSION = "2025.2.3"
+VERSION = "2025.3.0"
 LOGGER = logging.getLogger(__name__)
 
 
